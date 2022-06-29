de7e01c764acd4c3d434ebc50c30b489b5c3db22
PatyLuPz <1717110752@utectulancingo.edu.mx>
2022-06-28 21:01:17 -0500

    obtención de datos desde url y listado en pantalla

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+$url = "https://my-json-server.typicode.com/dp-danielortiz/dptest_jsonplaceholder/items";
+$data = json_decode(file_get_contents($url), true);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Prueba Uno</title>
+</head>
+
+<body>
+    <nav class="navbar bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                Prueba Uno
+            </a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Type</th>
+                    <th>Color</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                for ($x = 0; $x < count($data); $x++) {
+                ?>
+                    <tr>
+                        <td scope="row"><?php echo $data[$x]['id']; ?></td>
+                        <td><?php echo $data[$x]['type']; ?></td>
+                        <td><?php echo $data[$x]['color'];
+                        } ?></td>
+                    </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</html>
